2f6016b57353cf27599363f1fe7c4b7563e8585d
Gabriel <gabrielfunes100202@gmail.com>
2023-03-31 12:55:10 -0300

    Haciendo el puerto dinamico

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-
+const PORT = require('./constants.js')
 const app = express();
 
 app.get('/', function(req, res) {
     res.send('hello word')
 })
 
-app.listen(3000);
-console.log('Escuchando en el puerto 3000')
+app.listen(PORT);
+console.log(`Escuchando en el puerto: ${PORT}`)
